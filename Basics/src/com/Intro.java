5ade5b9dccdad3f58c664bcc8b88b123fc7bf177
bunny <satyabanni11851@gmail.com>
2025-03-02 19:33:39 +0530

    first commit

---
+++ Basics/src/com/Intro.java
@@ -0,0 +1,43 @@
+package com;
+
+public class Intro {
+	public static void main(String[] args) {
+		System.out.println("Hello,World!");
+		
+		//Data types
+		//Mobile Oneplus
+		String brand = "Oneplus";
+		int ram = 8;
+		double price = 24850.00;
+		long serialno = 1234567890;
+		boolean isAvailable = true;
+		float rating = 4.3f;
+		
+		//variables
+		int a = 10;
+//		where here "a" is a variable to store a value
+		
+		//literals (constant value)
+		int b = 20;
+//		Here 
+//		int=>data type
+//		b=>variable
+//		=:operator
+//		20=>literal(constant value)
+		
+		System.out.println(a);
+		
+		//binary 
+		int aa = 0b101;
+		System.out.println(aa);
+		
+		//hexa base 16 prefix 0x
+		int aaa = 0xf23;
+		System.out.println(aaa);
+		 
+		//octal base 8 prefix 0
+		int aaaa = 076;
+		System.out.println(aaaa);
+		
+	}
+}
