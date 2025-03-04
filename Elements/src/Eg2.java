e5671d8dc25fb9644f5c900b974d99ad9f8930c1
bunny <satyabanni11851@gmail.com>
2025-03-04 17:59:36 +0530

    Updated

--- Elements/src/Eg2.java
+++ Elements/src/Eg2.java
@@ -3,13 +3,20 @@ public class Eg2 {
 	//types of methods 
 	//even main method is static we can't change main method syntax
 	//to call instance method we must create object to it
-	
+	//to call static method we use class name
 	public static void main(String[] args) {
 		Eg2 eg=new Eg2();
 		eg.m1();
+		Eg2.M1();
 	}
 	
+	//Instance method
 	public void m1() {
-		System.out.println("M1 method running ");
+		System.out.println("Instance Method");
 	}
+	//Static Method
+	public static void M1() {
+		System.out.println("Static Method");
+	}
+	
 }
