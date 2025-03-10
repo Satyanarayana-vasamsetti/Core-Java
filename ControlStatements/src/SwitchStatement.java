759b1beca48f502f3d449e647af2052ae275ca4c
bunny <satyabanni11851@gmail.com>
2025-03-10 10:43:22 +0530

    Switch case

--- ControlStatements/src/SwitchStatement.java
+++ ControlStatements/src/SwitchStatement.java
@@ -1,4 +1,17 @@
 
 public class SwitchStatement {
-
+	public static void main(String[] args) {
+		int i =2;
+		switch(i) {
+		case 1:
+			System.out.println("Sunday");
+			break;
+		case 2:
+			System.out.println("Monday");
+			break;
+		default:
+			System.out.println("Invalid data");
+		}
+		
+	}
 }
