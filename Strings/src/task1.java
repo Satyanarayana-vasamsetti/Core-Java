ad0643c6f0285f062367169aea7f4176ecc67805
bunny <satyabanni11851@gmail.com>
2025-05-13 11:48:43 +0530

    creation of strings

---
+++ Strings/src/task1.java
@@ -0,0 +1,16 @@
+//String is a sequence of characters which is a immutable
+public class task1 {
+	public static void main(String[] args) {
+		//array approach 
+		char ch[] = {'a','e','i','o','u'};
+		System.out.println(ch);
+		
+		//String literal
+		String name = "Bunny";
+		System.out.println(name);
+		
+		//using new keyword
+		String s = new String("satya");
+		System.out.println(s);
+	}
+}
