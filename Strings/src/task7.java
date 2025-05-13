29a7d953dc89282f1fa02a1f41f457051334b45a
bunny <satyabanni11851@gmail.com>
2025-05-13 11:53:51 +0530

    StringBuilder vs StringBuffer vs String

---
+++ Strings/src/task7.java
@@ -0,0 +1,14 @@
+
+public class task7 {
+	public static void main(String[] args) {
+		String s = "bunny";
+		StringBuffer sb = new StringBuffer("satya");
+		StringBuilder sbd = new StringBuilder("Narayana");
+		System.out.println(sb);
+		System.out.println(sb.append("a"));
+		System.out.println(s);
+		System.out.println(s.concat(s));
+		System.out.println(sbd);
+		System.out.println(sbd.append("a"));
+	}
+}
