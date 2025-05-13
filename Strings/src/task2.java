7c21960692704829738d773ff424f41fa7bf3631
bunny <satyabanni11851@gmail.com>
2025-05-13 11:50:52 +0530

    length vs length()

---
+++ Strings/src/task2.java
@@ -0,0 +1,13 @@
+//length vs length()
+public class task2 {
+	public static void main(String[] args) {
+		char ch[] = {'b','u','n','n','y'};
+		System.out.println(ch.length);
+		
+		String s = "Satyanarayana";
+		System.out.println(s.length());
+		
+		String b = new String("Satyadevi");
+		System.out.println(b.length());
+	}
+}
