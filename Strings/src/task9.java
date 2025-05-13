618f66f747ed54a9c5188dc63b00d0b7d5016bf0
bunny <satyabanni11851@gmail.com>
2025-05-13 11:54:49 +0530

    equals() vs equalsIgnoreCase()

---
+++ Strings/src/task9.java
@@ -0,0 +1,11 @@
+
+public class task9 {
+	public static void main(String[] args) {
+		String s1 = "bunny";
+		String s2 = "bunny";
+		String s3 = "Bunny";
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s3));
+		System.out.println(s1.equalsIgnoreCase(s3));
+	}
+}
