87fdccfa1ea96e5ef8372be0b353dba24f59f89e
bunny <satyabanni11851@gmail.com>
2025-05-14 09:48:19 +0530

    finalkeyword as a variable

---
+++ finalkeyword/src/finalkeyword/finalasvar.java
@@ -0,0 +1,13 @@
+package finalkeyword;
+
+public class finalasvar {
+	public static void main(String[] args) {
+		int a = 10;
+		a = 11; //here reassign is possible
+		
+		final int b = 10;
+//		b=11;
+		//when we declare with final we can't reassign
+		
+	}
+}
