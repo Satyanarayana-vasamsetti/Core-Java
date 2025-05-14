e0f09135522b5e830d6fec05efc12858ce4a1afb
bunny <satyabanni11851@gmail.com>
2025-05-14 09:46:41 +0530

    final keyword as class

---
+++ finalkeyword/src/finalkeyword/finalasclass.java
@@ -0,0 +1,14 @@
+package finalkeyword;
+final class p {
+	void display() {
+		System.out.println("method");
+	}
+}
+//class b extends p{
+//	
+//}//error
+//because if we declare with final keyword we can't extend them
+
+public class finalasclass {
+
+}
