b1b815f5e31300fed74c7458414daa5c3046917b
bunny <satyabanni11851@gmail.com>
2025-05-14 09:47:59 +0530

    finalkeyword as a method

---
+++ finalkeyword/src/finalkeyword/finalasmeth.java
@@ -0,0 +1,16 @@
+package finalkeyword;
+class a{
+	final public void m1() {
+		
+	}
+}
+class b extends a{
+//	public void m1() {
+//		
+//	}//error
+	
+//because final method can't override
+}
+public class finalasmeth {
+
+}
