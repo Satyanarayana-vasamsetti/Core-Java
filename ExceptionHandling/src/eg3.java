428c95623ac6add2fbb218eeff0e61f147bc1539
bunny <satyabanni11851@gmail.com>
2025-05-16 09:59:55 +0530

    NullPointerException

---
+++ ExceptionHandling/src/eg3.java
@@ -0,0 +1,11 @@
+
+public class eg3 {
+	public static void main(String[] args) {
+		try {
+			String s = null;
+			System.out.println(s.length());
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
