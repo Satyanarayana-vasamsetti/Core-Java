8ce9b33c8920f59a224fd6b3ea8d6d6ed04083bf
bunny <satyabanni11851@gmail.com>
2025-05-16 10:00:33 +0530

    StringIndexOutOfBoundException

---
+++ ExceptionHandling/src/eg4.java
@@ -0,0 +1,11 @@
+public class eg4 {
+	public static void main(String[] args) {
+		try {
+			String s = "Java is a programming language";
+			System.out.println(s.length());
+			System.out.println(s.charAt(31));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
