cd67d71ffe0d0aee6809b9ed39c06baed45b7a39
bunny <satyabanni11851@gmail.com>
2025-05-16 10:02:59 +0530

    ClassNotFoundException

---
+++ ExceptionHandling/src/eg9.java
@@ -0,0 +1,10 @@
+class JDBC{
+	static {
+		System.out.println("jdbc");
+	}
+}
+public class eg9 {
+	public static void main(String[] args) throws ClassNotFoundException{
+		Class.forName("");				
+	}
+}
