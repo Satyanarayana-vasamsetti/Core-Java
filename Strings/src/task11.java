51cffc548d83c68eb28f9f698673eab57ac55d06
bunny <satyabanni11851@gmail.com>
2025-06-10 10:47:26 +0530

    String equals() and Object equals()

---
+++ Strings/src/task11.java
@@ -0,0 +1,12 @@
+
+public class task11 {
+	public static void main(String[] args) {
+		task11 t1 = new task11();
+		task11 t2 = new task11();
+		System.out.println(t1.equals(t2));
+		
+		String a = new String("A");
+		String b = new String("A");
+		System.out.println(a.equals(b));
+	}
+}
