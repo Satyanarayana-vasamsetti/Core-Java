b2a8f80a276a84cd4da3bbc143a089d69e0baf97
bunny <satyabanni11851@gmail.com>
2025-06-12 20:33:29 +0530

    for loop through array

---
+++ Arryas/src/arr6.java
@@ -0,0 +1,13 @@
+
+public class arr6 {
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5};
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		System.out.println(" ");
+		for(int j=a.length-1;j>=0;j--) {
+			System.out.println(a[j]);
+		}
+	}
+}
