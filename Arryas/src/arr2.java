81f58414dc5c7c878e349016db42bc5efd475a6e
bunny <satyabanni11851@gmail.com>
2025-06-12 20:31:31 +0530

    creation of array with values

---
+++ Arryas/src/arr2.java
@@ -0,0 +1,11 @@
+
+public class arr2 {
+	public static void main(String[] args) {
+		int[] i = {1,2,3,4,5};
+		System.out.println(i.length);
+		System.out.println(i[0]);
+		System.out.println(i[1]);
+		System.out.println(i[i.length]);//ArrayIndexOutOfBoundsException
+		
+	}
+}
