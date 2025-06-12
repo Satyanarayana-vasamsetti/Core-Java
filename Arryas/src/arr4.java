8d68f4dbd195b4f432319a9081ceaec638467176
bunny <satyabanni11851@gmail.com>
2025-06-12 20:32:29 +0530

    length of array

---
+++ Arryas/src/arr4.java
@@ -0,0 +1,12 @@
+
+public class arr4 {
+	public static void main(String[] args) {
+		int a[] = {1,2,3,4,5};
+		System.out.println(a.length);
+		System.out.println(a.length *2);
+		System.out.println(a.length + 3);
+		
+		System.out.println(a.length);
+		//length value is not change it remains constant
+	}
+}
