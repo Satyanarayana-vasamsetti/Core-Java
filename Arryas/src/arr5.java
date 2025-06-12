83ac9f4491f7b2c37845d101780d8304b357011e
bunny <satyabanni11851@gmail.com>
2025-06-12 20:32:59 +0530

    Negative array index exception

---
+++ Arryas/src/arr5.java
@@ -0,0 +1,12 @@
+
+public class arr5 {
+	public static void main(String[] args) {
+//		int[] arr = new int[-10];
+//		arr[-9]=100;
+//		System.out.println(arr[-8]);//NegativeArraySizeException
+		
+		int i[] = new int[0];
+		i[0]=100;
+		System.out.println(i[0]);//ArrayIndexOutOfBoundsException
+	}
+}
