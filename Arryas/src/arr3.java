1a2c21d7cb3bc5fa0214c5d59ed6f4fcf08710e8
bunny <satyabanni11851@gmail.com>
2025-06-12 20:32:03 +0530

    accessing of array elements

---
+++ Arryas/src/arr3.java
@@ -0,0 +1,16 @@
+
+public class arr3 {
+	public static void main(String[] args) {
+		int[] a = new int[5];
+//		for(int i=0;i<a.length;i++) {
+//			System.out.println(a[i]);
+//		}
+		a[0]=100;
+		a[1]=200;
+		a[2]=300;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+	}
+}
